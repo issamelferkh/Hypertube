af7a79d41a7515d7228df10a20c6d87cbde73d9b
Youness Benbraitit <benbraitit@gmail.com>
2021-03-22 15:41:22 +0100

    added darija language

---
+++ src/locale/ar.js
@@ -0,0 +1,282 @@
+var customLanguage = {
+    home: [
+      {
+        line1: "Tmata3 bi a7san aflam o siriyat fi  HyperTube",
+        line2: "clikat 9lal o hanta tma ...",
+        line3: "Fabor ma 3ndak ma t5alass rtaa7",
+        try_it: "JARAB DABA !"
+      }
+    ],
+    login: [
+      {
+        log_in: "Tconecta",
+        username: "Smiya",
+        password: "mot d pass",
+        login: "Tconecta",
+        other: "wla tconecta bi ..",
+        forgot_pwd: "Nsiti mot de pass tani?",
+        click_here: "Cliki hna",
+        no_account: "mazal ma 3ndak compte?",
+        register: "T9ayad"
+      }
+    ],
+    register: [
+      {
+        register: "T9ayad",
+        username: "Smiya",
+        firstname: "smiytak",
+        lastname: "Kniytak",
+        email: "Email",
+        password: "mot d pass",
+        repeat_pwd: "3awd lmot d pass",
+        other: "wla t9ayad bi..",
+        already_account: "deja 3ndk compte?",
+        login: "tconecta",
+        spaces: "mot de passe khass ykoun fih:",
+        lowercase: "7ourouf sghar",
+        uppercase: "7orouf kbar",
+        oneLetter: "A ",
+        oneNumber: "A ",
+        number: "number",
+        minimum: "Minimum ",
+        characters: "8 characters",
+        confirmation: "ra sifatna lik email!"
+      }
+    ],
+    forgot_password: [
+      {
+        title: "nsit mot de passe",
+        input_title: "smiytak wla email",
+        submit: "jabdoh liya",
+        invalid_username: "smiya ghalat",
+        invalid_unknown_email: "email ghalat wla ma kinch",
+        invalid_email: "email ghalat",
+        receive_email: "ghadi ywslk email!"
+      }
+    ],
+    reset_password: [
+      {
+        title: "mot de passe jdid",
+        pwd: "mot d pass",
+        repeat_pwd: "3awdo hna",
+        submit: "safi nadi kanadi",
+        unequal_passwords: "mot de passat machi b7al b7al",
+        reset_password_failed:
+          "mot de pass ma tbdlch 7awl man ba3d...",
+        reset_password_success: "mot de passe tbadl sir t7an"
+      }
+    ],
+    picture_modify: [
+      {
+        incorrect_format: "jib chi taswira mzn man tfalya",
+        incorrect_size: "jib chi tswira 9al man 2mb zayar..)",
+        incorrect_picture: "jib chi taswira correct..."
+      }
+    ],
+    delete_account: [
+      {
+        cancel: "anuli",
+        delete: "msa7",
+        delete_account_failed: "lcompte ma tamsa7ch",
+        delete_account_success: "lcompte tamsa7"
+      }
+    ],
+    activate_account: [
+      {
+        activation_success: "Compte dialk daba naaadi!",
+        activation_failed: "Error ma t activa walo...",
+        already_active: "had compte deja mactivi!"
+      }
+    ],
+    search: [
+      {
+        search: "9alab",
+        placeholder: "achman filam ka t9alb 3lih?",
+        rating: "Rating",
+        year: "l3am",
+        genre: "nou3"
+      }
+    ],
+    movie: [
+      {
+        select_source: "5tar mniin t5adam lfilam",
+        browser_support_error:
+          "browser dialk ma ki suportich 7awl t updati",
+        no_source: "ma kin ta source 3la 9bal had video",
+        theater_release: "Theater release:",
+        duration: "cha7al fih:",
+        director: "mo5rij:",
+        starring: "badya:",
+        synopsis: "Synopsis:"
+      }
+    ],
+    comments: [
+      {
+        placeholder: "5li chi commentaire hna...",
+        no_comment: "mazal ma kin ta comment...",
+        button: "sifat"
+      }
+    ],
+    navbar: [
+      {
+        register: "T9AYAD",
+        login: "D5OL",
+        logout: "5ROJ",
+        search: "9ALAB",
+        profile: "PROFIL"
+      }
+    ],
+    country_picker: [
+      {
+        title: "5tar dawla/lauga",
+        list_title: "ha lista"
+      }
+    ],
+    profile: [
+      {
+        language: "logha li ka t3jbk: ",
+        edit: "BDLHA",
+        movies_seen: "aflam li chayf",
+        edit_tooltip: "bdl profil",
+        following: "7adi",
+        no_movies: "mzl ma chafti 7ta film",
+        follow: "7di",
+        follow_tooltip: "tab3 had l3chir",
+        unfollow: "7ayd l7dya",
+        unfollow_tooltip: "7ayd l7dya man had 5ona",
+        following_user: "7adi",
+        follow_fail: "ma t9adch t7dih...",
+        unfollowing_user: "7ayd l7dya",
+        unfollow_fail: "ma t9adch t7yd l7adya....",
+        hold_on: "Tsana a jmi :)",
+        user_not_found: "ma kinch hada",
+        follow_already: "Rak deja 7adih",
+        unfollow_already: "wa 7ydti l7dya"
+      }
+    ],
+    edit_profile: [
+      {
+        title: "bdl profile",
+        username: "smiya",
+        firstname: "smiytk",
+        lastname: "knaytk",
+        language: "laugha:",
+        save: "39AL",
+        cancel: "5ROJ",
+        password: "MOT D PASS",
+        delete: "MSA7 LCOMPTE",
+        saved: "39AL",
+        nothing_changed: "ma tbadl walo",
+        incorrect_fields: "chi 7aja ma hiyach 9lb tani",
+        empty_email: "email 5awi 3mr "
+      }
+    ],
+    login_error: [
+      {
+        login_failed: "Error mni bghina nconectiwk...",
+        invalid_cred: "smiya wla mot d pass ma homach",
+        login_inactive: "had compte mzl ma t activa",
+        invalid_login: "d5l chi smiya mzna",
+        empty_login: "smiya ra 5awya 3mr...",
+        length_login: "smiya ra 7adha 30 7arf...zyr"
+      }
+    ],
+    register_error: [
+      {
+        register_failed: "erreur fach bghina n9ydouk...",
+        invalid_lastname: "kniya ma hiyach",
+        missing_lastname: "ktab knaytk",
+        space_lastname: "7ayd espace",
+        length_lastname: "kniya ra 7adha 30 7arf",
+        invalid_firstname: "smiytk ma hiyach",
+        missing_firstname: "ktb smiytk",
+        space_firstname: "7ayd espace",
+        length_firstname: "smiytk 7adha 30 7arf",
+        invalid_username: "smiya ma hiyach",
+        missing_username: "ktb smiytk",
+        space_username: "7ayd espace",
+        length_username: "smiya 7adha 30 7arf",
+        already_registered_username: "deja kayna sab9ok a wlidi",
+        invalid_email: "email ma howach",
+        missing_email: "ktab email",
+        space_email: "7ayd espace",
+        length_email: "email ra 7do 30 7arf",
+        already_registered_email: "email sab9oh lik",
+        invalid_password: "mot dpass ma howach",
+        missing_password: "ktb mot d pass",
+        space_password: "7ayd espace",
+        unequal_passwords: "mot d passat machi b7al b7al"
+      }
+    ],
+    update_user: [
+      {
+        register_failed: "Error fach bghina n9aydouk...",
+        invalid_lastname: "kniya mahiyach",
+        missing_lastname: "ktb kniytk",
+        space_lastname: "7ayd espace",
+        length_lastname: "knytk 7adha 30 7arf",
+        invalid_firstname: "smiytk ma hiyach",
+        missing_firstname: "ktb smiytk",
+        space_firstname: "7ayd espace",
+        length_firstname: "smiytk 7adha 30 7arf",
+        invalid_username: "smiya ma hiyach",
+        missing_username: "ktb smiytk",
+        space_username: "7ayd espace",
+        length_username: "smiya 7adha 30 7arf",
+        already_registered_username: "smiya deja msta3mla",
+        invalid_email: "email machi valid",
+        missing_email: "ktab chi l3ba hna",
+        space_email: "7ayd espace",
+        length_email: "email khass ykoun 7ado 30 7arf",
+        already_registered_email: "email deja msta3ml",
+        update_failed: "compte ma tchangach",
+        update_sucess: "compte tchanga"
+      }
+    ],
+    passwordSimple_error: [
+      {
+        invalid_pwd: "wa d5l chi mot de passe bi 3a9lo",
+        length_pwd: "mot de passe kha ykoun 7ado 30 7arf"
+      }
+    ],
+    firstname_error: [
+      {
+        no_space: "7ayd espace",
+        invalid_firstname: "smiya machi valid",
+        empty_firstname: "wa ktab chi la3ba hna",
+        length_firstname: "smiytak ra 7adha 30 7arf"
+      }
+    ],
+    lastname_error: [
+      {
+        no_space: "7ayd espace",
+        invalid_lastname: "kniya machi valid",
+        empty_lastname: "wa ktab chi la3ba hna",
+        length_lastname: "kniya ma khasch tfout 30 7arf"
+      }
+    ],
+    username_error: [
+      {
+        no_space: "smiya ma khasch ykoun fiha espacat",
+        invalid_username: "smiya ghalat khass ykoun ar9am o 7orouf safi",
+        empty_username: "bghiti t3tina smiya khawya hhh",
+        length_username:
+          "smiya twila bzf khassha tkon 30 max",
+        already_exist: "smiya deja kayn bdlha"
+      }
+    ],
+    email_error: [
+      {
+        no_space: "Email ma 5asch ykoun fih espace",
+        invalid_email: "email ma howach hanta :(example@email.com)",
+        empty_email: "email ma 5asch ykoun khawi",
+        length_email:
+          "mot de pass kha ykoun fih man 2 tal 30"
+      }
+    ],
+    pwd_match_error: "les mot d pass machi b7al b7al",
+    already_logged: "rak deja mconecti!"
+  };
+  
+  module.exports = customLanguage;
+  
